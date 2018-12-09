0fec2e709e26d9b0c7cbad2aaa50a36d64a0eee6
Michael Guterl <michael@diminishing.org>
2018-12-09 09:35:21 -0800

    Add gaming layout

--- keyboards/kbd6x/keymaps/mguterl/keymap.c
+++ keyboards/kbd6x/keymaps/mguterl/keymap.c
@@ -21,6 +21,7 @@
 #define QWER 0
 #define HHKB 1
 #define UTIL 2
+#define GAMING 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [QWER] = LAYOUT(
@@ -40,11 +41,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
 
   [UTIL] = LAYOUT(
-      RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      RESET,   TG(GAMING), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______, _______, _______,
       _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______,                   _______,                   _______, _______, _______
+      ),
+
+  [GAMING] = LAYOUT(
+      KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TG(GAMING),
+      KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                 KC_RGUI, KC_RALT, KC_RCTL
       ),
 };
 
