df7484cdc38072f098c46e6a651ba099eafcaf4e
Mike Guterl <guterlm@twitch.tv>
2019-02-20 09:05:41 -0800

    Esc tap, ctrl hold

--- keyboards/keebio/levinson/keymaps/mguterl/keymap.c
+++ keyboards/keebio/levinson/keymaps/mguterl/keymap.c
@@ -23,6 +23,7 @@ enum custom_keycodes {
 #define HPR_TAB ALL_T(KC_TAB) // Tap for Tab, hold for Hyper (Super+Ctrl+Shift+Alt)
 #define SFT_ENT MT(MOD_RSFT, KC_ENT)
 #define SPC_NAV LT(_NAV, KC_SPC)
+#define ESC_CTL MT(MOD_LCTL, KC_ESC)
 #define TG_GAME TG(_GAMING)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -40,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_ortho_4x12( \
   HPR_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  ESC_CTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
   KC_GESC, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   SPC_NAV, SPC_NAV, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
