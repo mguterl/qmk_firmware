f551eba0842899825dd87b393de6d7d323922656
Michael Guterl <michael@diminishing.org>
2021-01-21 16:25:27 -0800

    Fix media key positions

--- layouts/community/60_tsangan_hhkb/mguterl/keymap.c
+++ layouts/community/60_tsangan_hhkb/mguterl/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,\
     _______, _______, TG_WIN,  _______, TO_DFLT, _______, _______, _______, _______, _______, _______, KC_UP,   _______, KC_DEL, \
     KC_CAPS, _______, KC_VOLD, KC_VOLU, KC_MUTE, TG_GAME, KC_NO,   KC_NO,   KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,          _______, \
-    _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, _______, _______, _______, KC_END,  KC_PGDN, KC_DOWN,                   _______, _______,\
+    _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, _______, _______, KC_END,  KC_PGDN, KC_DOWN,                   _______, _______,\
     _______, _______, _______,                            _______,                            _______, _______, _______
   )
 };
